20c7f34487604931a70f52392625e2c3684eac73
unknown <mkonjkr@gmail.com>
2021-11-17 22:13:55 -0800

    First section

---
+++ src/Component/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="main_about">
+      <div className="aboutMe">ABOUT ME</div>
+      <div className="about_text">
+        Front-end developer who cares deeply about user experience. Serious
+        passion for UI design and new technologies.
+      </div>
+    </section>
+  );
+};
+
+export default About;
